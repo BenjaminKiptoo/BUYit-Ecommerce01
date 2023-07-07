e8b201b8d238d4293b6a71b69ea7ba2a9fcd95c1
Benjamin <kiptoo925@gmail.com>
2023-07-07 11:23:14 +0300

    Beginning admin_area on ECOMMERCE website

---
+++ Admin_area/index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin dashboard</title>
+    <!--------Bootstrap CSS link  ------------>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <!----------CSS file --------->
+    <link rel="stylesheet" href="../style.css">
+</head>
+<body>
+    <!----------navbar--------->
+    <div class="container-fluid p-0">
+        <!----------- first child ----------->
+        <nav class="navbar navbar-expand-lg navbar-light bg-info">
+            <div class="container-fluid">
+                <img src="../online-shop.png" alt="" class="logo">
+                <nav class="navbar navbar-expand-lg">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">Welcome guest</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </nav>
+
+        <!----------- second child -------------->
+        <div class="bg-light">
+            <h3 class="text-center p-2">Manage details</h3>
+        </div>
+
+        <!------------- third child ------------->
+        
+
+    </div>
+
+
+
+    <!--------Bootstrap link JS ------------>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+
+</body>
+</html>
